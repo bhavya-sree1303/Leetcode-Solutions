ff1e6cd2dd454cef129882fa2c1e003f27c65218
bhavya-sree1303 <bvemula@student.gitam.edu>
2026-07-12 15:22:34 +0530

    Time: 0 ms (100%), Space: 11.2 MB (92.97%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.c
@@ -0,0 +1,16 @@
+bool canJump(int* nums, int numsSize) {
+    int reach=0;
+    for(int i=0;i<numsSize;i++)
+    {
+        if(i>reach)
+        {
+            return 0;
+        }
+        if(i+nums[i]>reach)
+        {
+            reach=i+nums[i];
+        }
+    }
+    return 1;
+    
+}
