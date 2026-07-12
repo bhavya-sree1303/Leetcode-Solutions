587faffd668164dbea2d52a9b7b33bb3a186118a
bhavya-sree1303 <bvemula@student.gitam.edu>
2026-07-12 15:26:35 +0530

    Time: 101 ms (49.02%), Space: 9.4 MB (73.38%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.c
@@ -0,0 +1,23 @@
+int* twoSum(int* nums, int n, int target, int* returnSize)
+{
+    static int result[2];
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = i + 1; j < n; j++)
+        {
+            if(nums[i] + nums[j] == target)
+            {
+                result[0] = i;
+                result[1] = j;
+
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+
+    *returnSize = 0;
+    return result;
+}
+
