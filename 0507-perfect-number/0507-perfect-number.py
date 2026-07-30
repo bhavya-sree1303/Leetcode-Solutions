eaa7715f88ecf3157fb7770e57952210f1a2cdc2
bhavya-sree1303 <bvemula@student.gitam.edu>
2026-07-30 13:59:02 +0530

    Time: 11 ms (15.84%), Space: 12.5 MB (60.14%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def checkPerfectNumber(self, num):
+        if num ==1:
+            return False
+        s =1
+        i =2
+        while i * i <= num:
+            if num % i ==0:
+                s +=i
+                if i != num //i:
+                    s += num //i
+            i += 1
+        return s == num
