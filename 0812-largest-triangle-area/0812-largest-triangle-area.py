b8a5d46769b78940efc03e60a6d1fcb05cbee200
bhavya-sree1303 <bvemula@student.gitam.edu>
2026-08-01 14:06:28 +0530

    Time: 44 ms (83.23%), Space: 12.4 MB (55.06%) - LeetHub

--- 0812-largest-triangle-area/0812-largest-triangle-area.py
+++ 0812-largest-triangle-area/0812-largest-triangle-area.py
@@ -8,9 +8,7 @@ class Solution:
                 x2, y2 = points[j]
                 for k in range(j+1, n):
                     x3, y3 = points[k]
-                    area = abs(
-                        (x2 - x1)*(y3 - y1) - (x3 - x1)*(y2 - y1)
-                    ) / 2.0
+                    area = abs((x2 - x1)*(y3 - y1) - (x3 - x1)*(y2 - y1)) / 2.0
                     ans = max(ans, area)
 
         return ans
