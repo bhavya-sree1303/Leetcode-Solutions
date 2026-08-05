86f765ab459de2ce680b55e35a81949eb8394596
bhavya-sree1303 <bvemula@student.gitam.edu>
2026-08-05 12:14:52 +0530

    Time: 77 ms (37.6%), Space: 20.3 MB (85.81%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isMonotonic(self, nums):
+        inc = True
+        dec = True
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i-1]:
+                dec = False
+            if nums[i] < nums[i-1]:
+                inc = False
+        return inc or dec
