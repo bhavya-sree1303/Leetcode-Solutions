bec9e5e87902304007f7a8a8e53f5646b9bb9890
bhavya-sree1303 <bvemula@student.gitam.edu>
2026-08-08 20:23:26 +0530

    Time: 831 ms (42.86%), Space: 53.6 MB (42.86%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.py
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.py
@@ -1,14 +1,12 @@
 class Solution(object):
     def validSequence(self, word1, word2):
         n,m=len(word1),len(word2)
-        
         suf=[0]*(n+1)
         j=m-1
         for i in range(n-1,-1,-1):
             if j>=0 and word1[i]==word2[j]:
                 j-=1
             suf[i]=m-1-j
-        
         res=[]
         i=j=0
         used=False
