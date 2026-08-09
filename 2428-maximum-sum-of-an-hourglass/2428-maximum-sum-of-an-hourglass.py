cc66f2825729a3bee32b93e0e5a0ccd9647d4541
bhavya-sree1303 <bvemula@student.gitam.edu>
2026-08-09 21:43:18 +0530

    Time: 50 ms (24.55%), Space: 14.4 MB (25.15%) - LeetHub

---
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxSum(self, grid):
+        m = len(grid)
+        n = len(grid[0])
+        ans = 0
+        for i in range(m - 2):
+            for j in range(n - 2):
+                s = (
+                    grid[i][j] + grid[i][j+1] + grid[i][j+2] +
+                    grid[i+1][j+1] +
+                    grid[i+2][j] + grid[i+2][j+1] + grid[i+2][j+2]
+                )
+                ans = max(ans, s)
+        return ans
