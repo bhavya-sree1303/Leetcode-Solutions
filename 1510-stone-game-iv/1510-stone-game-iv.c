d4ad3f3cac38c3f4cf30cadc38d3a6b87bc1d657
bhavya-sree1303 <bvemula@student.gitam.edu>
2026-08-10 10:25:35 +0530

    Time: 20 ms (45.45%), Space: 8.4 MB (100%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.c
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+
+bool winnerSquareGame(int n) {
+    bool dp[n+1];
+    dp[0] = false;
+
+    for(int i=1;i<=n;i++){
+        dp[i]=false;
+        for(int j=1;j*j<=i;j++){
+            if(dp[i-j*j]==false){
+                dp[i]=true;
+                break;
+            }
+        }
+    }
+    return dp[n];
+}
